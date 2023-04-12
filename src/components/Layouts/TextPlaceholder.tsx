9991af80e05470182d531b8edd73dc148604b268
Lars Olson <eriklarsolson@yahoo.com>
2023-04-11 21:56:47 -0600

    expand text placeholder functionality

--- src/components/Layouts/TextPlaceholder.tsx
+++ src/components/Layouts/TextPlaceholder.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { ReactNode } from "react";
 import "./TextPlaceholder.css"
+
+const lineObjects: {
+    line: ReactNode
+}[] = [
+    {
+        line: <div className="melody-flex melody-items-center melody-w-full melody-space-x-2">
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-32"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+        </div>
+    },
+    {
+        line: <div className="melody-flex melody-items-center melody-w-full melody-space-x-2 melody-max-w-[480px]">
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
+        </div>
+    },
+    {
+        line: <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[400px]">
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-80"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+        </div>
+    },
+    {
+        line: <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[480px]">
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
+        </div>
+    },
+    {
+        line: <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[440px]">
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-32"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
+        </div>
+    },
+    {
+        line: <div className="melody-flex melody-items-center melody-w-full melody-space-x-2 melody-max-w-[360px]">
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+            <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-80"></div>
+            <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
+        </div>
+    },
+]
 
 export const TextPlaceholder = (props: {
     variant: string,
     size: string,
-    text: string
+    lines?: number
 }) => {
     const {
         variant,
         size,
-        text
+        lines = 6
     } = props
 
-    //TODO image placeholder https://flowbite.com/docs/components/skeleton/#image-placeholder
-    // widget placeholder https://flowbite.com/docs/components/skeleton/#widget-placeholder
+    //TODO widget placeholder https://flowbite.com/docs/components/skeleton/#widget-placeholder
 
     //TODO variant = 'blurb'?
     //<div role="status" class="max-w-sm animate-pulse">
@@ -29,36 +75,13 @@ export const TextPlaceholder = (props: {
     //variant = 'paragraph'
     return (
         <div role="status" className="melody-space-y-2.5 melody-animate-pulse melody-max-w-lg">
-            <div className="melody-flex melody-items-center melody-w-full melody-space-x-2">
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-32"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-            </div>
-            <div className="melody-flex melody-items-center melody-w-full melody-space-x-2 melody-max-w-[480px]">
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
-            </div>
-            <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[400px]">
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-80"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-            </div>
-            <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[480px]">
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
-            </div>
-            <div className="melody-flex melody-items-center w-full melody-space-x-2 melody-max-w-[440px]">
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-32"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-24"></div>
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-full"></div>
-            </div>
-            <div className="melody-flex melody-items-center melody-w-full melody-space-x-2 melody-max-w-[360px]">
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-                <div className="melody-h-2.5 melody-bg-gray-200 melody-rounded-full dark:melody-bg-gray-700 melody-w-80"></div>
-                <div className="melody-h-2.5 melody-bg-gray-300 melody-rounded-full dark:melody-bg-gray-600 melody-w-full"></div>
-            </div>
+
+            {Array.from(Array(lines).keys()).map(_ => {
+                const randomIndex: any = Math.floor(Math.random() * lines)
+                const randomLine: { line: ReactNode } = lineObjects[randomIndex]
+                return randomLine?.line
+            })}
+
             <span className="melody-sr-only">Loading...</span>
         </div>
     );
