eeb038b6650ff9c00d60c43d88ababfe7c031665
Lars Olson <eriklarsolson@yahoo.com>
2023-04-29 22:42:28 -0600

    fix required fields for radio button

--- src/components/Inputs/RadioButton.tsx
+++ src/components/Inputs/RadioButton.tsx
@@ -3,10 +3,10 @@ import {Label} from "../Layouts/Label";
 import React from "react";
 
 export const RadioButton = (props: {
-    variant: string,
-    size: string,
+    variant?: string,
+    size?: string,
     label?: string,
-    value: string
+    value: boolean
 }) => {
     const {
         variant,
@@ -22,10 +22,9 @@ export const RadioButton = (props: {
     return (
         <div className="melody-flex melody-items-center melody-mb-4">
 
-            <input id="radio-button-input"
-                   type="radio"
-                   value={"radio_value"} //TODO hardcoded
-                   checked={"radio_value" === value}
+            <input type="radio"
+                   // value={"radio_value"} //TODO hardcoded
+                   checked={value}
                    name="radio-button"
                    className="melody-w-4 melody-h-4 melody-text-blue-600 melody-bg-gray-100 melody-border-gray-300 focus:melody-ring-blue-500 dark:focus:melody-ring-blue-600 dark:melody-ring-offset-gray-800 focus:melody-ring-2 dark:melody-bg-gray-700 dark:melody-border-gray-600" />
 
