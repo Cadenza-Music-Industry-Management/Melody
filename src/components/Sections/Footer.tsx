00d06e7797f20505b1a338d25e1108796ef843dd
Lars Olson <eriklarsolson@yahoo.com>
2023-05-15 21:10:38 -0600

    add new footer design with wave

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -5,34 +5,43 @@ import { motion } from "framer-motion";
 
 export const Footer = () => {
     return (
-        <footer aria-label="Site Footer" className="melody-border-primary-100 melody-border-t melody-border-primary-100 melody-relative">
+        <footer aria-label="Site Footer" className="melody-bg-primary-100 melody-border-primary-100 melody-border-t melody-border-primary-100 melody-relative melody-overflow-hidden">
 
-            <div className={"melody-absolute melody-left-0 melody-right-0 melody-bottom-0 melody-z-[2]"}>
-                {/*Note: Generated from https://getwaves.io/*/}
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#9f8560" fillOpacity="1" d="M0,256L48,234.7C96,213,192,171,288,170.7C384,171,480,213,576,234.7C672,256,768,256,864,245.3C960,235,1056,213,1152,208C1248,203,1344,213,1392,218.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>            </div>
+            <div className={"melody-absolute melody-left-0 melody-right-0 melody-top-0 melody-z-[2]"}>
+                {/*Note: Generated from https://getwaves.io/ and expanded with https://www.photopea.com/*/}
+                <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4000 1500" width="4000"
+                     height="1500">
+                    <defs>
+                        <image width="4320" height="960" id="img1"
+                               href="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgMTQ0MCAzMjAiPjxwYXRoIGZpbGw9IiM5Zjg1NjAiIGZpbGwtb3BhY2l0eT0iMSIgZD0iTTAsMEwzMCwyNi43QzYwLDUzLDEyMCwxMDcsMTgwLDEyMi43QzI0MCwxMzksMzAwLDExNywzNjAsOTAuN0M0MjAsNjQsNDgwLDMyLDU0MCwzMkM2MDAsMzIsNjYwLDY0LDcyMCw3NC43Qzc4MCw4NSw4NDAsNzUsOTAwLDc0LjdDOTYwLDc1LDEwMjAsODUsMTA4MCw4NS4zQzExNDAsODUsMTIwMCw3NSwxMjYwLDg1LjNDMTMyMCw5NiwxMzgwLDEyOCwxNDEwLDE0NEwxNDQwLDE2MEwxNDQwLDMyMEwxNDEwLDMyMEMxMzgwLDMyMCwxMzIwLDMyMCwxMjYwLDMyMEMxMjAwLDMyMCwxMTQwLDMyMCwxMDgwLDMyMEMxMDIwLDMyMCw5NjAsMzIwLDkwMCwzMjBDODQwLDMyMCw3ODAsMzIwLDcyMCwzMjBDNjYwLDMyMCw2MDAsMzIwLDU0MCwzMjBDNDgwLDMyMCw0MjAsMzIwLDM2MCwzMjBDMzAwLDMyMCwyNDAsMzIwLDE4MCwzMjBDMTIwLDMyMCw2MCwzMjAsMzAsMzIwTDAsMzIwWiI+PC9wYXRoPjwvc3ZnPg==" />
+                    </defs>
+                    <path id="Shape 1" fill="#9f8560" d="m0 705.7h4000v1036.3h-4000z" />
+                    <use id="wave (4)" href="#img1" transform="matrix(.928,0,0,.928,0,0)" />
+                </svg>
+            </div>
 
-            <div className="melody-mx-auto melody-max-w-screen-xl melody-px-4 melody-py-16 sm:melody-px-6 lg:melody-px-8 melody-z-[3] melody-relative">
-                <div className="lg:melody-flex lg:melody-gap-8 melody-justify-center">
+            <div className="melody-mx-auto melody-z-[3] melody-relative melody-text-white">
+                <div className="lg:melody-flex lg:melody-gap-8 melody-justify-center melody-px-2 melody-py-16 sm:melody-px-6 lg:melody-px-8">
                     <div className="melody-mt-8 melody-grid melody-grid-cols-3 melody-gap-16 lg:melody-mt-0 lg:melody-grid-cols-3 lg:melody-gap-y-16">
                         <div className="melody-col-span-2 sm:melody-col-span-1">
-                            <p className="melody-font-medium melody-text-gray-900">Overview</p>
+                            <p className="melody-font-medium melody-underline">Overview</p>
 
                             <nav aria-label="Footer Navigation - Services" className="melody-mt-6">
                                 <ul className="melody-space-y-4 melody-text-sm">
                                     <li>
-                                        <Link href="/" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Home
                                         </Link>
                                     </li>
 
                                     <li>
-                                        <Link href="/pages/media" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/media" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Press & Media
                                         </Link>
                                     </li>
 
                                     <li>
-                                        <Link href="/pages/about" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/about" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             About Us
                                         </Link>
                                     </li>
@@ -41,7 +50,7 @@ export const Footer = () => {
                                         <Link href="https://cadenzamim.com/climate"
                                               rel="noopener noreferrer"
                                               target="_blank"
-                                              className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                              className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Climate
                                         </Link>
                                     </li>
@@ -50,18 +59,18 @@ export const Footer = () => {
                         </div>
 
                         <div className="melody-col-span-2 sm:melody-col-span-1">
-                            <p className="melody-font-medium melody-text-gray-900">Pricing</p>
+                            <p className="melody-font-medium melody-underline">Pricing</p>
 
                             <nav aria-label="Footer Navigation - Company" className="melody-mt-6">
                                 <ul className="melody-space-y-4 melody-text-sm">
                                     <li>
-                                        <Link href="/pages/pricing" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/pricing" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Subscriptions
                                         </Link>
                                     </li>
 
                                     <li>
-                                        <Link href="/pages/refund" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/refund" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Refund Policy
                                         </Link>
                                     </li>
@@ -70,67 +79,34 @@ export const Footer = () => {
                         </div>
 
                         <div className="melody-col-span-2 sm:melody-col-span-1">
-                            <p className="melody-font-medium melody-text-gray-900">Help</p>
+                            <p className="melody-font-medium melody-underline">Help</p>
 
                             <nav aria-label="Footer Navigation - Company" className="melody-mt-6">
                                 <ul className="melody-space-y-4 melody-text-sm">
                                     <li>
-                                        <Link href="/pages/support" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/support" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Support
                                         </Link>
                                     </li>
 
                                     <li>
-                                        <Link href="/pages/contact" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/contact" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Contact Us
                                         </Link>
                                     </li>
 
                                     <li>
-                                        <Link href="/pages/changelog" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">
+                                        <Link href="/pages/changelog" className="melody-text-gray-200 melody-transition hover:melody-opacity-75">
                                             Changelog
                                         </Link>
                                     </li>
                                 </ul>
                             </nav>
                         </div>
-
-                        {/*<div className="melody-col-span-2 sm:melody-col-span-1">*/}
-                        {/*    <p className="melody-font-medium melody-text-gray-900">Legal</p>*/}
-
-                        {/*    <nav aria-label="Footer Navigation - Legal" className="melody-mt-6">*/}
-                        {/*        <ul className="melody-space-y-4 melody-text-sm">*/}
-                        {/*            <li>*/}
-                        {/*                <Link href="#" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">*/}
-                        {/*                    Terms Of Service*/}
-                        {/*                </Link>*/}
-                        {/*            </li>*/}
-
-                        {/*            <li>*/}
-                        {/*                <Link href="#" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">*/}
-                        {/*                    Privacy Policy*/}
-
-                        {/*                </Link>*/}
-                        {/*            </li>*/}
-
-                        {/*            <li>*/}
-                        {/*                <Link href="#" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">*/}
-                        {/*                    Cookies*/}
-                        {/*                </Link>*/}
-                        {/*            </li>*/}
-
-                        {/*            <li>*/}
-                        {/*                <Link href="#" className="melody-text-gray-700 melody-transition hover:melody-opacity-75">*/}
-                        {/*                    User Guidelines*/}
-                        {/*                </Link>*/}
-                        {/*            </li>*/}
-                        {/*        </ul>*/}
-                        {/*    </nav>*/}
-                        {/*</div>*/}
                     </div>
                 </div>
 
-                <div className="melody-mt-8 melody-border-t melody-border-gray-300 melody-pt-8 melody-text-white">
+                <div className="melody-mt-8 melody-border-t melody-border-gray-300 melody-pt-8 melody-bg-white melody-px-2 melody-py-16 sm:melody-px-6 lg:melody-px-8">
                     <div className="sm:melody-flex sm:melody-justify-between">
                         <p className="melody-text-xs melody-text-gray-700">
                             &copy; 2023. Cadenza Music Industry Management LLC. All rights reserved.
