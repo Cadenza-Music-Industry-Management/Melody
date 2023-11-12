f6399fcb92995de131e7a06b33f2195e24a6bd71
Lars Olson <eriklarsolson@yahoo.com>
2023-11-12 08:59:02 -0700

    Replace all uses of old generated form component with better specific ones, updating tos/privacy pages, and introduced footer notification component for Melody to make legal agreement a footer note instead of a popup

---
+++ src/components/Layouts/FooterNotification.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { ReactNode } from "react";
+
+export function FooterNotification(props: {
+    visible: boolean,
+    title?: string,
+    subComponent: ReactNode
+}) {
+
+    const {
+        visible,
+        title,
+        subComponent
+    } = props
+
+    return (
+        <div className={`melody-fixed melody-bottom-0 melody-left-0 melody-w-full melody-bg-secondary-100 melody-text-white melody-z-[1000] ${visible ? 'melody-block' : 'melody-hidden'}`}>
+            <div className="melody-container melody-mx-auto melody-p-2">
+                <div className="flex justify-between items-center">
+                    <div>
+                        {title && <h2 className="melody-text-lg melody-font-semibold">{title}</h2>}
+                        {subComponent}
+                    </div>
+
+                {/*    TODO button here custom text for "accept"*/}
+
+                </div>
+            </div>
+        </div>
+    )
+}
